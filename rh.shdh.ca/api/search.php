e7dbaffb313badcfe4e9dfe9a32900c35c92147c
resumehive <resumehive@nighthawk.dreamhost.com>
2015-10-18 14:45:34 -0700

    Updated sources so angular now interfacing with db and api

---
+++ rh.shdh.ca/api/search.php
@@ -0,0 +1,32 @@
+<?php
+   header('Content-type: application/json');
+   $searchTerm = $_GET["term"];
+
+   $servername = "rhdb.shdh.ca";
+   $username = "rhdb";
+   $password = "_dubHacks2015*";
+   $db = "resumehive";
+
+   $conn = new mysqli($servername, $username, $password, $db);
+
+   if($conn->connect_error){
+      die("Connection failed: " .$conn->connect_error);
+   }else{
+      $query = "SELECT filename, date_added, category FROM `files` ";
+      $splitterms = explode(" ", $searchTerm);
+      for($i = 0; $i < sizeof($splitterms); $i++){
+         if($i == 0)
+            $query = $query . "WHERE `fulltext` LIKE '%${splitterms[$i]}%' ";
+         else
+            $query = $query . "OR `fulltext` LIKE '%${splitterms[$i]}%' ";
+      }
+      //echo $query;
+      $result = mysqli_query($conn, $query);
+      $data = array();
+      while($row = $result->fetch_row()){
+         $array = array("name" => $row[0], "date" => $row[1], "category" => $row[2]);
+         $data[] = $array;
+      }
+      echo json_encode(array("length" => sizeof($data), "data" => $data));
+   }
+?>
--- rh.shdh.ca/js/app.js
+++ rh.shdh.ca/js/app.js
@@ -1,7 +1,26 @@
 var rhApp = angular.module('rhapp', []);
 
-rhApp.controller('MainPageController', function($scope){
+rhApp.controller('MainPageController', function($scope, $http){
    console.log("TEST");
-   $scope.test = [
+   $scope.text = '';
+   $scope.data = [
    ];
+   $scope.getNumber = function(){
+      return new Array($scope.text.length);
+   };
+   $scope.search = function(){
+      console.log($scope.text);
+      var request = $http({
+         method: "get",
+         url: "api/search.php",
+         params: {
+           action: "get",
+           term: $scope.text
+         }
+      }).then(function successCallback(response){
+         console.log(response.data);
+         var data = response.data;
+         $scope.data = data.data;
+      })
+   };
 });
